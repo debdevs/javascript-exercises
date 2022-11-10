6e2b349949f221d51a9d128cb7048aca353a4cf0
debdevs <debdevs97@gmail.com>
2022-11-09 23:50:48 -0500

    create repeat string function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (input, number) {
+  let text = '';
+  for (let i = 0; i < number; i++) {
+    text += input;
+  }
+  return text;
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
