17fe5184fa0bbefa9bb932794a3edbdf67608050
debdevs <debdevs97@gmail.com>
2022-11-09 23:58:31 -0500

    create working string reversal function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  text = '';
+  let text_array = input.split('');
+  for (let i = text_array.length - 1; i > -1; i--) {
+    text += text_array[i];
+  }
+  return text;
 };
 
 // Do not edit below this line
