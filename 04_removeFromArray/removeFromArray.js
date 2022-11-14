aedff59172e7c118b9215373d36a0d0347b0a0b0
debdevs <debdevs97@gmail.com>
2022-11-14 13:10:34 -0500

    add variadic functions base

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (input, number) {
+  const array = input;
+  const args = Array.from(arguments);
+  arr = array.filter((item) => item !== number);
 
+  let longest = '';
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i].length > longest.length) {
+      longest = arguments[i];
+    }
+  }
+  return longest, arr;
 };
 
 // Do not edit below this line
