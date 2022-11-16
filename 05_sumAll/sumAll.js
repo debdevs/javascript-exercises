053bb93d0fc79dfa47e24fe56062c6d5249a19f2
debdevs <debdevs97@gmail.com>
2022-11-16 01:37:06 -0500

    fix error for array input
    changed sumAllfuntion to output error upon input of array.
    
    Changed if statements to start with checking if input is a number. All other types will output 'ERROR'

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,8 @@
 const sumAll = function (first, second) {
   /* console.log(Math.max(...arguments)) */
+
   let prev_val = [];
+
   for (let i = 1; i < Math.max(...arguments) + 1; i++) {
     prev_val.push(i);
 
@@ -12,25 +14,31 @@ const sumAll = function (first, second) {
 
   var nums = prev_val;
   var sum = 0;
+  //   console.log(prev_val);
+  if (Number.isInteger(first) == false || Number.isInteger(second) == false) {
+    sum = 'ERROR';
+  } else if (
+    Number.isInteger(first) == true ||
+    Number.isInteger(second) == true
+  ) {
+    sum = sum;
 
-  for (var n = 0; n < nums.length; n++) {
-    sum += parseInt(nums[n]);
-    if (
-      first < 0 ||
-      second < 0 ||
-      Number.isInteger(first) == false ||
-      Number.isInteger(second) == false
-    ) {
-      console.log(first);
-      sum = 'ERROR';
-    } else if (first || second > 0) {
-      sum = sum;
+    for (var n = 0; n < nums.length; n++) {
+      sum += parseInt(nums[n]);
+
+      if (first < 0 || second < 0) {
+        sum = 'ERROR';
+      } else {
+        console.log('TE', sum);
+        sum = sum;
+      }
     }
   }
+
   // npm test sumAll.spec.js
   return sum;
 };
 
-console.log(sumAll(1, 9));
+console.log(sumAll(1, [1, 9]));
 // Do not edit below this line
 module.exports = sumAll;
