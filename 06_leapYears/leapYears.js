cfdffce1f0053ef345b87d92dbdfda069bd6894b
debdevs <debdevs97@gmail.com>
2022-11-16 02:24:34 -0500

    create pseudocode for leapYears
    solved leapYears by using pseudocode and translating it to if statements

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let output = null;
+  if (year % 4 == 0 && year % 100 != 0) {
+    output = true;
+  } else if (year % 4 == 0 && year % 400 == 0) {
+    output = true;
+  } else {
+    output = false;
+  }
+  return output;
 };
 
+console.log(leapYears(1996));
+//npm test leapYears.spec.js
 // Do not edit below this line
 module.exports = leapYears;
