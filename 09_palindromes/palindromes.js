89fb5fa2611e76e7bac59f11c60a82197da6da89
debdevs <deborahmdaniels1@gmail.com>
2022-12-13 11:47:39 -0500

    update palindromes
    test added replace for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,12 @@
 const palindromes = function (str) {
     let input = str
     let compare = input.split("").reverse().join("")
-    let compare_result = input.split(",").reverse().join("").toLowerCase()
+    let compare_result = input.split(",").reverse().join("").toLowerCase().replace(".","")
 
 
 
     let reverse = str.split("").reverse().join("")
-    let result = str.split(",").join("").toLowerCase()
+    let result = str.split(",").join("").toLowerCase().replace(".","")
     let status = null
 
     if (compare_result == result){
