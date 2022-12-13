fdca026b062454d315ed8e689a79e16fdeee53da
debdevs <deborahmdaniels1@gmail.com>
2022-12-13 11:40:20 -0500

    create palindromes base function
    used string split, reverse, and join to compare and reverse strings

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let input = str
+    let compare = input.split("").reverse().join("")
+    let compare_result = input.split(",").reverse().join("").toLowerCase()
+
+
+
+    let reverse = str.split("").reverse().join("")
+    let result = str.split(",").join("").toLowerCase()
+    let status = null
+
+    if (compare_result == result){
+        status = true
+    } else{
+        status = false
+    }
+
+    return status
 
 };
 
