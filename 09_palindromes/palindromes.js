406ec7bb240d25a87fbf2369a40741bdf4244330
debdevs <deborahmdaniels1@gmail.com>
2022-12-13 12:06:29 -0500

    finish palindromes js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -3,20 +3,22 @@ const palindromes = function (str) {
     let input = str
    
     let compare_result = str.split(",").join("").toLowerCase().replace(".","")
-
 	let compare_result_1 = compare_result.split("").reverse().join("")
+    let compare_result_2 = compare_result_1.split("!").join("")
 
 
     let result = str.split(",").join("").toLowerCase().replace(".","")
+    let result_2 = result.split("!").join("")
     let status = null
 
-    if (compare_result == result){
+    if (compare_result_2.split(" ").join("") == result_2.split(" ").join("")){
         status = true
     } else{
         status = false
     }
-  console.log(compare_result_1.split(" ").join(""))
-	console.log(result.split(" ").join(""))
+    console.log(compare_result_2.split(" ").join(""))
+	console.log(result_2.split(" ").join(""))
+    console.log(status)
     return status
     
 
